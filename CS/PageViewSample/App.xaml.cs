ab92eb34c9887a5b5c5e5987b039815116f59de7
Anthony Sam <anton.sameishchev@devexpress.com>
2021-08-10 12:32:28 +0300

    Update App.xaml.cs

--- CS/PageViewSample/App.xaml.cs
+++ CS/PageViewSample/App.xaml.cs
@@ -10,6 +10,12 @@ namespace PageViewSample {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application {
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ThemeManager.ApplicationThemeName = Theme.Office2013Name;
+            base.OnStartup(e);
+        }
     }
 }
